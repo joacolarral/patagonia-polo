6237afa61a231b1d74641df6dd4614e2c6856b27
Joaquin Larralde <joaquinlarralde@Joaquins-MacBook-Pro.local>
2021-09-13 16:58:50 -0300

    feat: contact us maquetado finished

--- src/App/App.js
+++ src/App/App.js
@@ -8,6 +8,7 @@ import AboutPP from './AboutPP/AboutPP';
 import Location from './Location/Location';
 import Courses from './Courses/Courses';
 import Gallery from './Gallery/Gallery';
+import ContactUs from './ContactUs/ContactUs';
 
 function App() {
   const [language, setLanguage] = useState('es');
@@ -29,6 +30,7 @@ function App() {
       <Location />
       <Courses content={infoSections.courses} lang={language} />
       <Gallery content={infoSections.gallery} lang={language} />
+      <ContactUs content={infoSections.contact_us} lang={language} />
     </div>
   );
 }
---
+++ src/App/ContactUs/ContactUs.js
@@ -0,0 +1,57 @@
+const ContactUs = (props) => {
+  const { content, lang } = props;
+  const { title, subtitle, input_name, input_mail, input_message } = content;
+  return (
+    <div id="contact-us">
+      <div className="container">
+        <h1>{title[lang]}</h1>
+        <h2>{subtitle[lang]}</h2>
+        <div className="info-container">
+          <div className="form-container">
+            <div className="inputs-container">
+              <div className="input-box">
+                <label htmlFor="name" className="prefix">
+                  {input_name[lang]}
+                </label>
+                <input id="name" name="name" />
+              </div>
+              <div className="input-box">
+                <label htmlFor="mail" className="prefix">
+                  {input_mail[lang]}
+                </label>
+                <input id="mail" name="mail" />
+              </div>
+            </div>
+            <div className="textarea-box">
+              <label htmlFor="message" className="prefix">
+                {input_message[lang]}
+              </label>
+              <textarea rows="7" id="message" name="message" />
+            </div>
+          </div>
+          <div className="external-contact">
+            <div className="space-blank" />
+            <div className="social-media">
+              <div className="icons">TE</div>
+              <div>+54 9 11 58030952</div>
+            </div>
+            <div className="social-media">
+              <div className="icons">MA</div>
+              <div>patagoniapoloweek@gmail.com</div>
+            </div>
+            <div className="social-media">
+              <div className="icons">IG</div>
+              <div>patagoniapolo</div>
+            </div>
+            <div className="social-media">
+              <div className="icons">FB</div>
+              <div>Patagonia Polo Week</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactUs;
